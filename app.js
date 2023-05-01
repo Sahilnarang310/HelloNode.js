2ea484cd2f9b89505f9a931ea362913a7c09ecf9
Sahilnarang310 <softwaredev310@gamil.com>
2023-05-01 12:24:19 +0530

    Answer of task 1 in starting with Express Framework

--- app.js
+++ app.js
@@ -1,6 +1,15 @@
-const http = require('http');
-const routes = require('./task9');
 
-const server = http.createServer(routes.handler);
-console.log(routes.someText);
-server.listen(3000);
+const express=require('express');
+const app=express();
+// console.log(routes.SomeText);
+app.use((req, res, next)=>{
+    console.log('in the middleware');
+    next();  // This allows the request to continue to the next middleware in line.
+});
+app.use((req, res, next)=>{
+    console.log('in the another middleware');
+   res.send('<h1> Hello from EXpress </h1>');
+});
+
+app.listen(3000);
+ 
